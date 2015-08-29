7aece65343605ec1d32943795cc82ae695221b1a
Peter Hellstrand <phe@capraconsulting.no>
2015-08-29 23:26:27 +0200

    work in progress

--- scripts/lookup.js
+++ scripts/lookup.js
@@ -1,14 +1,39 @@
 
 
+var domaninsEndings = ['.no', '.se', '.com'];
 
-$.ajax({
-	url: '/ds/check.cgi?domainname=capraconsulting.no',
-	success: function (data) {
-		debugger;
-	}, 
-	error: function (data) {
-		debugger;
-	}
-});
+function getResultForDomains() {
+	var domainnames = Array.prototype.slice.call(arguments);
+	var promises = [];
+	domainnames.forEach(function (domainname) {
+		domaninsEndings.forEach(function (domaninsEnding) {
+			var promise = getResultForDomain(domainname, domaninsEnding);
+			promises.push(promise);
+		});
+	});
+	$.when.all(promises).then(function(schemas) {
+		console.log("DONE", this, schemas); // 'schemas' is now an array
+	}, function(e) {
+		console.log("My ajax failed");
+	});
+};
 
 
+function getResultForDomain(domainname, domaninsEnding) {
+	return $.ajax({
+		url: '/ds/check.cgi',
+		data:{
+			domainname: domainname.concat(domaninsEnding)
+		},
+		success: function (data) {
+			console.log('success', data);
+		},
+		error: function (data) {
+			console.log(data);
+		}
+	});
+}
+
+
+
+getResultForDomains('capraconsulting', 'volvo');
