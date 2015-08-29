c0c60082b4baa10af4f18050f4a3387dd520ba88
Markus Loupeen <mal@capra.me>
2015-08-29 23:46:44 +0200

    Added table creator

--- scripts/input-parser.js
+++ scripts/input-parser.js
@@ -1,12 +1,28 @@
 (function() {
+	function createTable(domains, tlds) {
+		var table = $('<table><thead><tr></tr></thead><tbody></tbody></table>');
+		tlds.forEach(function(tld) {
+			$(table).find('thead tr').append('<th>' + tld + '</th>');
+		});
+
+		domains.forEach(function(domain) {
+			var row = $('<tr id="' + domain + '"></tr>');
+			row.append($('<td>' + domain + '</td>'));
+			tlds.forEach(function(tld) {
+				row.append($('<td class="' + tld + '"></td>'));
+			});
+		});
+		return table;
+	}
+
 	function inputParser(text) {
 		var domains = text.split(/\n/);
 		getResultForDomains(domains);
 	}
-
 	$('#domain-checker').bind('submit', function(e) {
 		e.preventDefault();
 		var data = $('#domain-checker').find('textarea').val();
 		inputParser(data);
+		$('body').append(createTable(data, domaninsEndings));
 	});
 })();
