bd00179906fc88fc8dfb29860458afac180ca4f2
Peter Hellstrand <phe@capraconsulting.no>
2015-08-29 23:39:48 +0200

    connected

--- scripts/input-parser.js
+++ scripts/input-parser.js
@@ -2,8 +2,6 @@
 	function inputParser(text) {
 		var domains = text.split(/\n/);
 		getResultForDomains(domains);
-		console.log(domains);
-		// TODO : Phe
 	}
 
 	$('#domain-checker').bind('submit', function(e) {
