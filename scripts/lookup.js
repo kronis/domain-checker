c3271a415ffb292ea090727fa9157438f4e1a3cf
Peter Hellstrand <phe@capraconsulting.no>
2015-08-29 21:12:21 +0200

    work in progress

---
+++ scripts/lookup.js
@@ -0,0 +1,12 @@
+
+
+
+$.ajax({
+    url: 'https://www.domeneshop.no/check.cgi?domainname=capraconsulting.no',
+    dataType: 'jsonp',
+    success: function (data) {
+       //  console.log('' + data)
+    }
+});
+
+
